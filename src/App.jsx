7265b54e12bff116e2262ea219966bc8f7671d97
Tim Yang <timmac@tims-mbp.lan>
2022-04-20 03:16:20 -0700

    installed tailwind and added boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import './App.css';
+import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <p>Hello</p>
+      <div className="container-xl mx-auto justify-items-center min-w-900 max-w-7xl h-screen drop-shadow-[0_0px_35px_rgba(0,0,0,0.15)] bg-white">
+        <Home />
+      </div>
     </>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className='h-full pt-20'>
+      <p className='text-8xl text-center'>Peter & Grace</p>
+      <image src="" />
+    </div>
+  );
+}
+
+export default Home;
