7dced6f26c74d327d335a5568668e6bc03407b94
Tim Yang <timmac@tims-mbp.lan>
2022-04-21 08:00:39 -0700

    fixed bg

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 function App() {
   return (
     <>
-      <div className="bg-[url('./assets/background.png')] md:bg-[url('./assets/background2.png')] bg-fixed bg-no-repeat bg-cover md:bg-contain">
+      <div className="bg-[url('./assets/background.png')] md:bg-[url('./assets/background2.png')] bg-fixed bg-no-repeat bg-cover">
         <div className="container-xl mx-auto justify-items-center min-w-900 max-w-7xl h-full drop-shadow-[0_0px_35px_rgba(0,0,0,0.15)] bg-[rgb(254,254,254,0.60)] z-10">
           <Home />
         </div>
