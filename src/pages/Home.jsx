b2f4911253d74cfb789cc87be1522f3699a22313
Tim Yang <timmac@tims-mbp.lan>
2022-04-26 04:58:04 -0700

    added youtubelive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
+import Youtubelive from "../components/Youtubelive";
+
 function Home() {
   return (
-    <section className="container-xl mx-auto justify-items-center min-w-900 max-w-7xl h-screen drop-shadow-[0_0px_35px_rgba(0,0,0,0.15)] bg-[rgb(254,254,254,0.60)] z-10">
-      <h1 className="text-7xl text-center pt-20 md:pt-40">Peter & Grace</h1>
-      <image src="" />
+    <section className="flex container mx-auto align-center justify-center max-w-7xl h-screen drop-shadow-[0_0px_35px_rgba(0,0,0,0.15)] bg-[rgb(254,254,254,0.60)]">
+      <div className="flex flex-col w-full">
+        <h1 className="w-full text-center text-7xl pt-20 md:pt-40">Peter & Grace</h1>
+        <Youtubelive />
+      </div>
     </section>
   );
 }
