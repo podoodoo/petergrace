6cd197509a989a8ed62a13fa3c5c5f04c592c4f5
Tim Yang <timmac@tims-mbp.lan>
2022-04-26 05:38:06 -0700

    updated bg

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Rsvp from "./pages/Rsvp";
 function App() {
   return (
     <>
-      <div className="bg-[url('./assets/background.png')] md:bg-[url('./assets/background2.png')] bg-fixed bg-no-repeat bg-cover">
+      <div className="bg-[url('./assets/background.png')] sm:bg-[url('./assets/background2.png')] bg-no-repeat bg-cover bg-fixed">
         <Home />
         <Rsvp />
       </div>
