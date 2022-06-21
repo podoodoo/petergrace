6d0599e1e4626e3ad26872411c916567f2ca8660
Tim Yang <timmac@tims-mbp.lan>
2022-06-21 01:10:53 -0700

    minor home and info adjustments

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,20 @@ import pic7 from "../assets/pics/7.jpg";
 function Home() {
   return (
     <div className="flex flex-col w-full h-full align-center">
-      <h1 className="w-full text-center text-4xl mb-10 pt-16 md:pt-24 md:text-6xl">
+      <h1 className="w-full text-center text-4xl pt-16 md:pt-12 md:text-6xl">
         Peter & Grace
       </h1>
-      <div className="flex justify-center align-middle overflow-hidden m-5 md:m-20">
-        <img src={pic7} alt="7.jpg" className="md:w-3/4 md:object-none md:object-[-270px_-750px]" />
+      <div className="flex mx-auto justify-center align-middle overflow-hidden m-5 md:m-12 md:h-2/3 md:w-7/8">
+        <img
+          src={pic7}
+          alt="7.jpg"
+          className="md:w-3/4 md:object-none md:object-[45%_75%]" // -270px_-750px
+        />
       </div>
-      <h3 className="text-center mt-10 mx-5 px-5 pt-5 text-2xl border-t-2 md:mx-20">
+      <h2 className="text-center mt-10 mx-5 px-5 text-2xl md:mx-20 md:mt-0">
         "Love is patient, love is kind..."
-      </h3>
-      <h4 className="text-center mt-5 mx-5 px-5">- 1 Corinthians 13:4</h4>
+      </h2>
+      <h4 className="text-center mx-5 px-5">- 1 Corinthians 13:4</h4>
     </div>
   );
 }
--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -1,26 +1,28 @@
 function Info() {
   return (
-    <div className="flex flex-col w-full h-full align-center">
+    <div className="flex flex-col w-full h-full align-center justify-center">
       <h2 className="w-full text-center text-4xl mb-8 pt-4 md:text-6xl md:pt-40">
         Wedding Day
       </h2>
-      <h3 className="text-sm text-center mb-4 mx-5 py-5 px-8 border-t-2 md:px-48 md:mx-20">
+      <h3 className="text-sm text-center mx-5 py-5 px-8 md:px-48 md:mx-20">
         Each of us alone is incomplete, together with Christ we are as one in
         this there shall be joy. We, Grace Kim & Peter Bae, invite you to share
         in this joy on{" "}
         <span className="text-xl block my-3">July 30th, 2022</span> when we will
         exchange marriage vows and ask God's blessing on our union at 11AM.
       </h3>
-      <h3 className="text-sm text-center mb-16 mx-5 py-5 px-8 border-y-2 md:px-44 md:mx-20">
+      <div className="flex mx-auto my-4 border-t border-black w-1/3 justify-center"></div>
+      <h3 className="text-sm text-center mb-16 mx-5 py-5 px-8 md:px-44 md:mx-20">
         은혜 안에서 만난 저희 두 사람이 믿음의 가정을 이루는 첫걸음을
         내딛습니다. 하나님의 사랑을 주위에 나누며 살아가는 가정이 되도록 축복과
         격려로 함께하여 주시면 감사하겠습니다.
       </h3>
-      <div className="text-center justify-center mx-5 pb-7 mb-7 border-b md:mx-20">
+      <div className="text-center justify-center mx-5 md:mx-20">
         <h3 className="text-2xl mb-4">Ceremony</h3>
         <h5 className="text-bold">Legacy Church</h5>
         <p className="text-center">9012 160 St, Surrey, BC V4N 3A5, Canada</p>
       </div>
+      <div className="flex mx-auto my-8 border-t w-1/3 justify-center"></div>
       <div className="text-center justify-center mx-5 md:mx-20">
         <h3 className=" text-2xl mb-4">Reception</h3>
         <h5 className="text-center text-bold">Sheraton Hotel Guildford</h5>
