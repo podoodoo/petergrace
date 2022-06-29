66c9e9ac338b232dd799170dc213d54484914523
Tim Yang <timmac@Tims-MacBook-Pro.local>
2022-06-28 21:54:05 -0700

    height fight home

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <>
       <main className="bg-[url('./assets/background.png')] md:bg-[url('./assets/background2.png')] bg-no-repeat md:bg-cover bg-fixed">
         <div className="flex container justify-center align-center m-auto flex-col h-full max-w-7xl drop-shadow-[0_0px_20px_rgba(0,0,0,0.25)] bg-[rgb(254,254,254,0.75)]">
-          <section className="h-full w-full">
+          <section className="w-full h-screen">
             <Home />
           </section>
           <section className="w-full mb-10 mt-10 md:mt-0">
