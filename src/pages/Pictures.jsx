36ef6409cad05918e375de6c2e7fb6992ae1d4b6
Tim Yang <timmac@tims-mbp.lan>
2022-07-12 13:28:29 -0700

    adjusted picture position

--- src/pages/Pictures.jsx
+++ src/pages/Pictures.jsx
@@ -6,8 +6,7 @@ function Pictures() {
           <img
             src={require("../assets/pics/landing/top1.jpg")}
             alt="top1.jpg"
-            className="w-full h-full absolute inset-0 object-cover"
-            loading="lazy"
+            className="w-full h-full absolute inset-0 object-cover object-[0%_67%]"
           />
         </div>
         <div className="relative">
@@ -15,7 +14,6 @@ function Pictures() {
             src={require("../assets/pics/landing/top3.jpg")}
             alt="top3.jpg"
             className="w-full h-full absolute inset-0 object-cover object-bottom"
-            loading="lazy"
           />
         </div>
         <div className="relative">
@@ -31,7 +29,6 @@ function Pictures() {
             src={require("../assets/pics/landing/top5.jpg")}
             alt="top5.jpg"
             className="w-full h-full absolute inset-0 object-cover"
-            loading="lazy"
           />
         </div>
       </div>
